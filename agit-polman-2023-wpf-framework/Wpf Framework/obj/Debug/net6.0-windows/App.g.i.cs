b16ec95a6863b38497932265c55a09f308baacb9
student <m.jlbrn21@gmail.com>
2023-07-04 09:21:16 +0700

    Baru bay

--- agit-polman-2023-wpf-framework/Wpf Framework/obj/Debug/net6.0-windows/App.g.i.cs
+++ agit-polman-2023-wpf-framework/Wpf Framework/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Wpf_Framework {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Wpf_Framework {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public static void Main() {
             Wpf_Framework.App app = new Wpf_Framework.App();
             app.InitializeComponent();
